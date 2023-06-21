eae53f5e92c2b6ea0d0d3415d349a5653cdfa930
ValentinCasas <101840863+ValentinCasas@users.noreply.github.com>
2023-06-21 19:59:36 -0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EXPEDITEE_REST.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# EXPEDITEE_REST
+ api rest - expeditee- felivery - laboratorio 3
